31edf0fd0e5e3ed5ceebf97585b3b33346e96dc5
Jett B Pittman <jett@jettpittman.com>
2023-02-25 16:05:54 -0600

    add DB stats

---
+++ scripts/db-stats.js
@@ -0,0 +1,8 @@
+fetch("https://api.sagh-st.org/info")
+        .then(response => response.json())
+        .then(json => {
+            let sp = document.getElementById("db-stats");
+            let p = document.createElement("p");
+            p.innerText = `Athletes - ${json['athletes']}\nMeets - ${json['meets']}\nEntries - ${json['entries']}`;
+            sp.appendChild(p);
+        })
