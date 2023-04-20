0cd5367f07edc5161dbaf9e688b844ac9c0a45f6
Jett B Pittman <jett@jettpittman.com>
2023-04-19 21:41:45 -0500

    optimize meet colors

--- scripts/get-meets.js
+++ scripts/get-meets.js
@@ -1,38 +1,3 @@
-venue_colors = {
-    "AH": "F8FFB0",
-    "AM": "FFCCCC",
-    "BU": "FFCCFF",
-    "CC": "99CCFF",
-    "CO": "99CCFF",
-    "NE": "CCCCFF",
-    "NS": "66DD88",
-    "SA": "FFCC99",
-    "SW": "99FFCC",
-    "TM": "FFFFFF",
-    "UT": "FFCC99",
-    "PA": "99CCFF",
-    "DC": "99CCFF",
-    "LC": "99CCFF",
-    undefined: "99CCFF"
-}
-
-venues = {
-    "AH": "Alamo Heights Natatorium",
-    "AM": "Texas A&M Student Recreation Center Natatorium",
-    "BU": "YMCA of the Highland Lakes",
-    "CC": "Corpus Cristi ISD Natatorium",
-    "CO": "Coronado Pool",
-    "NE": "Josh Davis Natatorium/Bill Walker Pool",
-    "NS": "NISD Natatorium & Swim Center",
-    "SA": "San Antonio Natatorium",
-    "SW": "Southwest Aquatic Center",
-    "TM": "TMI Episcopal",
-    "UT": "Lee & Joe Jamail Texas Swimming Center",
-    "PA": "Palo Alto Natatorium",
-    "LC": "Lamar CISD Natatorium",
-    "DC": "Don Cook Natatorium"
-}
-
 function getMID (param) {
     if (param) {
         const urlParams = new URLSearchParams(window.location.search);
